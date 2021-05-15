d4ca23b18fe4590e13967cc1ae979821ebf35ac9
CaiuPelado <caiomorais99@outlook.com>
2021-05-14 22:52:15 -0300

    comentário

--- JavaServer/src/javaserver/Cliente.java
+++ JavaServer/src/javaserver/Cliente.java
@@ -2,6 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ * Olá professor lindo do meu coração, em dias de desespero, se desespere.
  */
 package javaserver;
 
